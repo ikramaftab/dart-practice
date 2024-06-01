edcac798a944a5a496e2cc63ef9f5e222746d73a
Ikram <bhattiikram78@gmail.com>
2024-06-01 11:49:44 -0700

    added lecture 10-11-12

---
+++ lecture 10-11-12.dart
@@ -0,0 +1,11 @@
+void main () {
+  int years = 10;
+  int weeks;
+  int days;
+  weeks = years * 52;
+  days = weeks * 7;
+  print("years : $years");
+  print("weeks : $weeks");
+  print("days : $days");
+
+}
